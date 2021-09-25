f5761d9666c61f1efce8b18fefacf2423b01c434
shah-priyansh <shahpriyansh902705@gmail.com>
2021-09-25 16:19:14 +0530

    fix :navbar header

--- src/compomtion/Navbar.js
+++ src/compomtion/Navbar.js
@@ -8,7 +8,7 @@ export class Navbar extends Component {
       <div>
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
           <div className="container-fluid">
-            <h5>Navbar</h5>
+            <h5>Daily News</h5>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
